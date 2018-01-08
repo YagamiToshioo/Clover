1bd0c21b6565adc6768d03623922493665c37099
YagamiToshioo <34960152+YagamiToshioo@users.noreply.github.com>
2018-01-09 00:12:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ Modified by Ilyasa
 */
 require_once('./line_class.php');
 
-$channelAccessToken = 'YOUR-CHANNEL-ACCESS-TOKEN'; //Your Channel Access Token
-$channelSecret = 'YOUR-CHANNEL-SECRET';//Your Channel Secret
+$channelAccessToken = D9a/QCVaAtse1+NGys5Jpqs95kyN9Y+sxqyPiMT/wxNELWXXqqT87V7YiWXsfrXEoDG5BnreW5LcnunFWpjWve/aWbu0Cjq8b63dUYorc4zTiWaDiGb6Ueev/ZL2RhQfgaCwhd4CLEs2vDrO5nSUWQdB04t89/1O/w1cDnyilFU='; //Your Channel Access Token
+$channelSecret = 'ab8f924bed71417feb058ce1c1464193';//Your Channel Secret
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 
@@ -18,78 +18,24 @@ $profil = $client->profil($userId);
 $pesan_datang = $message['text'];
 
 if($message['type']=='sticker')
-{	
-	$balas = array(
-							'UserID' => $profil->userId,	
-                                                        'replyToken' => $replyToken,							
-							'messages' => array(
-								array(
-										'type' => 'text',									
-										'text' => 'Terima Kasih Stikernya.'										
-									
-									)
-							)
-						);
-						
+<script>
+url = "https://www.cleverbot.com/getreply";
+key = "CC6a2Wzi4dqrDF0c-GdaQTGdN6Q";
+input = encodeURIComponent ("How are you?");
+function ProcessReply (data) {
+    if (data.error) console.log ('Error: ' + data.error);
+    else alert ('Reply: ' + data.output);
 }
-else
-$pesan=str_replace(" ", "%20", $pesan_datang);
-$key = 'YOUR-API-KEY-SIMSIMI'; //API SimSimi
-$url = 'http://sandbox.api.simsimi.com/request.p?key='.$key.'&lc=id&ft=1.0&text='.$pesan;
-$json_data = file_get_contents($url);
-$url=json_decode($json_data,1);
-$diterima = $url['response'];
-if($message['type']=='text')
-{
-if($url['result'] == 404)
-	{
-		$balas = array(
-							'UserID' => $profil->userId,	
-                                                        'replyToken' => $replyToken,													
-							'messages' => array(
-								array(
-										'type' => 'text',					
-										'text' => 'Mohon Gunakan Bahasa Indonesia Yang Benar :D.'
-									)
-							)
-						);
-				
-	}
-else
-if($url['result'] != 100)
-	{
-		
-		
-		$balas = array(
-							'UserID' => $profil->userId,
-                                                        'replyToken' => $replyToken,														
-							'messages' => array(
-								array(
-										'type' => 'text',					
-										'text' => 'Maaf '.$profil->displayName.' Server Kami Sedang Sibuk Sekarang.'
-									)
-							)
-						);
-				
-	}
-	else{
-		$balas = array(
-							'UserID' => $profil->userId,
-                                                        'replyToken' => $replyToken,														
-							'messages' => array(
-								array(
-										'type' => 'text',					
-										'text' => ''.$diterima.''
-									)
-							)
-						);
-						
-	}
-}
- 
-$result =  json_encode($balas);
+script_element = document.createElement('script'); //create new script element
+script_element.src = url + "?input=" + input + '&key=' + key + '&callback=ProcessReply';
+document.getElementsByTagName ('head')[0].appendChild(script_element); //append to page, which executes it
+</script>
 
-file_put_contents('./reply.json',$result);
-
-
-$client->replyMessage($balas);
+<script>
+$.ajax({
+    url: url,
+    data: {"input": input, "key": key}, //input automatically encoded
+    dataType: "jsonp",
+    success: ProcessReply
+});
+</script>
